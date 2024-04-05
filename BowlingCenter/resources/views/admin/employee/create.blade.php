21cdea3610b277cf70e0ad4b3dbe9f429d5cfa64
Steff Brouwer <sbrouwer05@gmail.com>
2024-04-05 15:55:52 +0200

    edit function error

--- BowlingCenter/resources/views/admin/employee/create.blade.php
+++ BowlingCenter/resources/views/admin/employee/create.blade.php
@@ -1,26 +1,26 @@
 <x-admin-layout>
-<form action="{{ route('admin.employee.create') }}" method="post">
-    @csrf
-    @method('PUT')
-    <div class="form-group">
-        <label for="name">Name</label>
-        <input type="text" name="name" id="name" class="form-control" required>
-    </div>
-
-    <div class="form-group">
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email" class="form-control" required>
-    </div>
-
-    <div class="form-group">
-        <select name="usertype" id="usertype">
-            <option value="3" default>gebruiker</option>
-            <option value="2">Medewerker</option>
+    <form action="{{ route('admin.employee.create') }}" method="post">
+        @csrf
+        @method('PUT')
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name" class="form-control" required>
+        </div>
+    
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" class="form-control" required>
+        </div>
+    
+        <div class="form-group">
+            <select name="usertype" id="usertype">
+                <option value="3" default>gebruiker</option>
+                <option value="2">Medewerker</option>
+            </select>
+        </div>
+    
         </select>
-    </div>
-
-    </select>
-
-    <button type="submit" class="btn btn-primary">Create</button>
-</form>
-</x-admin-layout>
+    
+        <button type="submit" class="btn btn-primary">Create</button>
+    </form>
+    </x-admin-layout>

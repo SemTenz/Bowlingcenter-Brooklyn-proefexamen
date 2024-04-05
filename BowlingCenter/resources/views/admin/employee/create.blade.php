b506fb7a2ecdca058dd4e1aa2e43db918e82f2a4
Steff Brouwer <sbrouwer05@gmail.com>
2024-04-05 15:42:25 +0200

    error create function

--- BowlingCenter/resources/views/admin/employee/create.blade.php
+++ BowlingCenter/resources/views/admin/employee/create.blade.php
@@ -1,6 +1,7 @@
-<form action="{{ route('admin.employees.store') }}" method="POST">
+<x-admin-layout>
+<form action="{{ route('admin.employee.create') }}" method="post">
     @csrf
-
+    @method('PUT')
     <div class="form-group">
         <label for="name">Name</label>
         <input type="text" name="name" id="name" class="form-control" required>
@@ -12,16 +13,14 @@
     </div>
 
     <div class="form-group">
-    <select name="usertype" id="usertype">
-
-        <option selected="selected" value="{{$users->usertype}}">
-            {{$users->usertype}}
-        </option>
-        <option value="3" default>gebruiker</option>
-        <option value="2">Medewerker</option>
+        <select name="usertype" id="usertype">
+            <option value="3" default>gebruiker</option>
+            <option value="2">Medewerker</option>
+        </select>
     </div>
 
     </select>
 
     <button type="submit" class="btn btn-primary">Create</button>
 </form>
+</x-admin-layout>

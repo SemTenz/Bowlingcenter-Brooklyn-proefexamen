b79b3eeed7e5ab65437560f59e0895232e751134
Axel van_beek <336019@student.mboutrecht.nl>
2024-04-05 15:56:53 +0200

    merge conflict

---
+++ BowlingCenter/resources/views/admin/employee/index.blade.php
@@ -0,0 +1,37 @@
+<x-admin-layout>
+
+
+    <table>
+        <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>email</th>
+            <th>usertype</th>
+
+
+        </tr>
+
+
+        @foreach ($users->Users as $user)
+        <tr>
+            <td>{{ $user->id }}</td>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+            <td>{{ $users->usertype }}</td>
+            <td><a href="{{route('admin.employee.edit',$user->id)}}">wijzig</a></td>
+            <td>
+                <form action="{{ route('admin.employee.delete', $user->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+
+
+
+
+        @endforeach
+        <a href="{{route('admin.employee.create',$user->id)}}">aanmaken</a>
+    </table>
+</x-admin-layout>

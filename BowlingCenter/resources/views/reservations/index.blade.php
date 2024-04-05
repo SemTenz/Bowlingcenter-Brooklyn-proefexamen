6ab18ecf9146fc3d9f4a44da26831532d4b1396d
Bram <336796@student.mboutrecht.nl>
2024-04-05 16:00:53 +0200

    delete alert bericht

--- BowlingCenter/resources/views/reservations/index.blade.php
+++ BowlingCenter/resources/views/reservations/index.blade.php
@@ -79,8 +79,54 @@
                         <td>{{ $reservation->people }}</td>
                         <td>{{ $reservation->phoneNumber }}</td>
 
+                        <td>
+                            @if ($reservation->options_id >= 1)
+                                {{ $reservation->options_id }}
+                            @else
+                                niet van toepassing
+                            @endif
+                        </td>
+                        
+                        <td><a href="{{ route('reservations.edit', $reservation->id) }}" class="btn btn-primary">Edit</a></td>
+                        <td>
+                            <form action="{{ route('reservations.destroy', $reservation->id) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger" onclick="return confirm('Weet je zeker dat je dit wilt verwijderen?')">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    @endsection
+</x-app-layout>
+@extends('layouts.app')
+<x-app-layout>
+    @section('content')
+    <div class="container">
+        <h1>My Reservations</h1>
+
+        <table>
+            <th>datum en tijd </th>
+            <th>naam </th>
+            <th>aantal personen </th>
+            <th>telefoonnummer </th>
+
+            <th>extra optie</th>
+
+
+            @foreach ($reservations as $reservation)
+            @foreach ($options as $option)
+            <tr>
+                <td>{{ $reservation->date }} {{ $reservation->time }}</td>
+                <td>{{ $reservation->name }}</td>
+                <td>{{ $reservation->people }}</td>
+                <td>{{ $reservation->phoneNumber }}</td>
+
                 @if ($reservation->options_id >= 1)
-                <td>{{ $reservation->options_id }}</td>
+                <td>{{ $option->option }}</td>
                 @else
                 <td>niet van toepassing</td>
                 @endif
@@ -94,7 +140,9 @@
                 </td>
             </tr>
             @endforeach
+            @endforeach
         </table>
     </div>
     @endsection
+
 </x-app-layout>
